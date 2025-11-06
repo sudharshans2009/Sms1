e7b22e259de33b1b65234dd4de192ab89d24212a
Karthikeyan G <216304420+karthikeyan006867@users.noreply.github.com>
2025-11-06 12:34:13 +0000

    changess

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config, { isServer }) => {
+    // Handle leaflet on server side
+    if (isServer) {
+      config.externals = [...(config.externals || []), 'leaflet', 'react-leaflet'];
+    }
+    return config;
+  },
+  // Suppress webpack warnings for leaflet
+  transpilePackages: ['react-leaflet', 'leaflet'],
 }
 
 module.exports = nextConfig
